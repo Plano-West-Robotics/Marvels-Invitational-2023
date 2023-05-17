bdbff13acf193784e98a9aeab95990a55e032f71
Apoorv <74080457+Apoorv-Agrawal24@users.noreply.github.com>
2023-05-16 20:09:30 -0500

    Added Lift Subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Lift.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.subsystem;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.inchworm.PIDController;
+
+public class Lift {
+    static int currentPos;
+    static boolean manual;
+
+    final public static int POS_HIGH = 0;
+    final public static int POS_MID = 0;
+    final public static int POS_LOW = 0;
+    final public static int POS_DOWN = 0;
+    final public static int MAX_HEIGHT = 0;
+
+    PIDController pid;
+    DcMotor leftSlide;
+    DcMotor rightSlide;
+
+    Telemetry telemetry;
+
+    public Lift(Telemetry telemetry, HardwareMap hw) {
+        pid = new PIDController(0, 0, 0, currentPos);
+        pid.setTarget(currentPos);
+
+        leftSlide = hw.get(DcMotor.class, "leftSlide");
+        rightSlide = hw.get(DcMotor.class, "rightSlide");
+
+        this.telemetry = telemetry;
+    }
+
+    void setPower(double power) {
+        leftSlide.setPower(power);
+        rightSlide.setPower(power);
+    }
+
+    public void goTo(int target) {
+        pid.setTarget(target);
+        pid.reset();
+    }
+
+    public void setManual(boolean manual) {
+        Lift.manual = manual;
+        pid.reset();
+    }
+
+    public void update(double stickVal) {
+        if (Lift.manual) {
+            setPower(stickVal);
+        } else {
+            setPower(pid.calculate(currentPos));
+        }
+
+        currentPos = leftSlide.getCurrentPosition();
+    }
+}
