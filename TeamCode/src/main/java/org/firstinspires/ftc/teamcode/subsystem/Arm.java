11f529b94836d9004a2180bc2a2b3a149924814d
Apoorv <74080457+Apoorv-Agrawal24@users.noreply.github.com>
2023-05-18 18:11:21 -0500

    Created Arm subsystem.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Arm.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.subsystem;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arm {
+    Servo leftServo;
+    Servo rightServo;
+    Servo wristServo;
+
+    Telemetry telemetry;
+
+    public static final List POS_PICKUP = new ArrayList<Double>() {
+        {
+            add(0d); // left servo
+            add(0d); // right servo
+            add(0d); // wrist servo
+        }
+    };
+
+    public static final List POS_MID = new ArrayList<Double>() {
+        {
+            add(0d); // left servo
+            add(0d); // right servo
+            add(0d); // wrist servo
+        }
+    };
+
+    public static final List POS_DROP = new ArrayList<Double>() {
+        {
+            add(0d); // left servo
+            add(0d); // right servo
+            add(0d); // wrist servo
+        }
+    };
+
+    public Arm(Telemetry telemetry, HardwareMap hw) {
+        leftServo = hw.get(Servo.class, "armLeftServo");
+        rightServo = hw.get(Servo.class, "armRightServo");
+        wristServo = hw.get(Servo.class, "wristServo");
+        this.telemetry = telemetry;
+    }
+
+    public void goTo(List pos) {
+        try {
+            leftServo.setPosition(pos.indexOf(0));
+            rightServo.setPosition(pos.indexOf(1));
+            wristServo.setPosition(pos.indexOf(2));
+        } catch (Exception exception) {
+            throw new RuntimeException("Please use the preset positions");
+        }
+    }
+}
